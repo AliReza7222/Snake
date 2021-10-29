acff2b74e35d65b50bce811933a0357e8cb2abfc
aliReza <ali.r.salehi99@gmail.com>
2021-10-29 22:07:44 +0330

    start project snake

---
+++ snake.py
@@ -0,0 +1,34 @@
+import consts
+
+
+class Snake:
+
+    dx = {'UP': 0, 'DOWN': 0, 'LEFT': -1, 'RIGHT': 1}
+    dy = {'UP': -1, 'DOWN': 1, 'LEFT': 0, 'RIGHT': 0}
+
+    def __init__(self, keys, game, pos, color, direction):
+        self.keys = keys
+        self.cells = [pos]
+        self.game = game
+        self.game.add_snake(self)
+        self.color = color
+        self.direction = direction
+        game.get_cell(pos).set_color(color)
+
+    def get_head(self):
+        return self.cells[-1]
+
+    def val(self, x):
+        if x < 0:
+            x += self.game.size
+
+        if x >= self.game.size:
+            x -= self.game.size
+
+        return x
+
+    def next_move(self):
+        pass
+
+    def handle(self, keys):
+        pass
